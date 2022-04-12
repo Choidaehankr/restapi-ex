ef1ba75e705c2594067c961f7b7f0494caa9fdcb
Choidaehan <71701634+Choidaehankr@users.noreply.github.com>
2022-04-13 02:28:26 +0900

    setting

---
+++ test/src/main/java/restapi/ex/sample/sampleapi/common/response/ApiException.java
@@ -0,0 +1,17 @@
+package restapi.ex.sample.sampleapi.common.response;
+
+
+import lombok.Getter;
+import restapi.ex.sample.sampleapi.common.enums.ExceptionEnum;
+
+@Getter
+public class ApiException extends RuntimeException {
+    private ExceptionEnum error;
+
+    // public ApiException(ExceptionEnum e) throws ApiException {
+    public ApiException(ExceptionEnum e) {
+        super(e.getMessage());
+        this.error = e;
+        // throw new ApiException(ExceptionEnum.SECURITY_01);
+    }
+}
